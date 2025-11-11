0ba7520857e785c839187e0169d5a18bc789f51e
VIJAY ASHOK KARLAWAD <139671897+VIJAY-A-K-1405@users.noreply.github.com>
2025-11-11 12:03:35 +0530

    Time: 809 ms (10.29%) | Memory: 0B (100.00%) - LeetSync

--- 1811-fix-names-in-a-table/fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -1,7 +1,11 @@
-# Write your MySQL query statement below
 # The CONCAT() function is widely supported across various SQL databases (MySQL, PostgreSQL, SQL Server, etc.). It takes multiple string expressions as arguments and joins them together.
+# The substr() is a function or method that extracts a portion of a string by specifying a starting position and an optional length.
+
 select user_id , 
 concat(upper(left(name, 1)), lower(right(name, length(name)-1))) as name
 from users 
 order by user_id
-  
+
+
+# select user_id, concat(upper(substr(name,1,1)), lower(substr(name,2))) as name  from users 
+# order by user_id
