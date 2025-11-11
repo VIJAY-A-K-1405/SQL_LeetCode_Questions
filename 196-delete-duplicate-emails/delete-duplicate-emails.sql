923069968affaab6dd954d9e5a4686ef994a2369
VIJAY ASHOK KARLAWAD <139671897+VIJAY-A-K-1405@users.noreply.github.com>
2025-11-11 12:16:38 +0530

    Time: 523 ms (91.43%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,9 +1,13 @@
-# Write your MySQL query statement below
-# Solution to delete all duplicate emails, keeping only one unique email with the smallest id.
-# For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
+--  Write your MySQL query statement below
+--  Solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+--  For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
 
-delete p1
-from person p1
-inner join person p2
-on p1.email = p2.email
-and p1.id > p2.id
+-- delete p1
+-- from person p1
+-- inner join person p2
+-- on p1.email = p2.email
+-- and p1.id > p2.id
+
+
+-- select distinct email from Person;
+ delete from Person P1 where P1.id not in (select * from (select min(id) from Person P2 group by P2.email) as sub );
