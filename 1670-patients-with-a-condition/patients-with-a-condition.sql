75dd7bf1dbfe818af6a35fe17ad97c739cb2a397
VIJAY ASHOK KARLAWAD <139671897+VIJAY-A-K-1405@users.noreply.github.com>
2025-11-13 04:26:08 +0530

    Time: 365 ms (50.65%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
+-- The LIKE operator is used in a WHERE clause to search for a specified pattern in a column.
+
+-- There are two wildcards often used in conjunction with the LIKE operator:
+
+-- The percent sign % represents zero, one, or multiple characters
+-- The underscore sign _ represents one, single character
+
 select * from Patients
 where conditions like 'DIAB1%'
 or conditions like '% DIAB1%'
