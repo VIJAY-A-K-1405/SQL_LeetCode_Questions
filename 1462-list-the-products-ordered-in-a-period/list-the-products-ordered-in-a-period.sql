29ea4a4f93c2be3414b66a1291a51ac639f316db
VIJAY ASHOK KARLAWAD <139671897+VIJAY-A-K-1405@users.noreply.github.com>
2025-11-15 11:44:00 +0530

    Time: 712 ms (39.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1462-list-the-products-ordered-in-a-period/list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+-- The EXTRACT function in SQL is used to retrieve a specific component from a date or time value. 
+
+select p.product_name , sum(o.unit) as unit
+from products p
+left join orders o 
+on p.product_id = o.product_id
+where extract(year_month from  o.order_date) = 202002
+group by o. product_id
+having sum(o.unit) >= 100;
