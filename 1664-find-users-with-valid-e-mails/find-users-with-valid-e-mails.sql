3fea753fcbb0a82a72d1a6aa19dbf2cffc5b1b46
VIJAY ASHOK KARLAWAD <139671897+VIJAY-A-K-1405@users.noreply.github.com>
2025-11-15 12:10:36 +0530

    Time: 718 ms (49.62%) | Memory: 0B (100.00%) - LeetSync

--- 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/find-users-with-valid-e-mails.sql
@@ -5,6 +5,6 @@
 -- select * from users
 -- where mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$'
 
-SELECT user_id, name, mail FROM Users
-WHERE mail REGEXP '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$'
-AND BINARY RIGHT(mail, 13) = '@leetcode.com';
+select *
+from Users
+where regexp_like(mail,'^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$','c');
