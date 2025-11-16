97cf1b67ab45c1602c14d8682a5ed0e15d18d1e4
VIJAY ASHOK KARLAWAD <139671897+VIJAY-A-K-1405@users.noreply.github.com>
2025-11-16 11:22:21 +0530

    Time: 344 ms (47.86%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -2,6 +2,17 @@
 -- The DATEADD() function adds a time/date interval to a date and then returns the date.
 -- The DATE_SUB() function subtracts a time/date interval from a date and then returns the date.
 
+with cte as 
+(select visited_on, sum(amount) as daily_amount
+from Customer
+group by visited_on)
+
+select visited_on,
+(sum(daily_amount) over (order by visited_on rows between 6 preceding and current row)) as amount,
+round(avg(daily_amount) over (order by visited_on rows between 6 preceding and current row), 2) as average_amount
+from cte
+
+limit 1000 offset 6
 
 -- select visited_on, 
 -- (
@@ -20,16 +31,3 @@
 -- )
 -- group by visited_on
 -- order by visited_on
-
-
-with cte as 
-(select visited_on, sum(amount) as daily_amount
-from Customer
-group by visited_on)
-
-select visited_on,
-(sum(daily_amount) over (order by visited_on rows between 6 preceding and current row)) as amount,
-round(avg(daily_amount) over (order by visited_on rows between 6 preceding and current row), 2) as average_amount
-from cte
-
-limit 1000 offset 6
