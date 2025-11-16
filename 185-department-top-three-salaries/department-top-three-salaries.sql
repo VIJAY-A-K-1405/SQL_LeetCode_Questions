345b3a2cf1f86fb1f328efa650920529d2e28823
VIJAY ASHOK KARLAWAD <139671897+VIJAY-A-K-1405@users.noreply.github.com>
2025-11-16 12:53:03 +0530

    Time: 798 ms (91.82%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -3,12 +3,27 @@
 -- Unlike RANK(), it avoids gaps by giving tied rows consecutive ranks, ensuring a continuous sequence. 
 -- It is especially useful when a clear, uninterrupted ranking is needed for accurate analysis and reporting.
 
-select Department , Employee , Salary from 
-(select d.name as Department , 
-e.name as Employee , 
-e. salary as Salary,
-dense_rank() over (partition by d.name order by e.salary desc) as ranks
-from employee e
-left join Department d 
-on e.departmentId = d.id) v
-where ranks <=3
+WITH employee_department AS
+    (
+    SELECT d.id, 
+        d.name AS Department, 
+        salary AS Salary, 
+        e.name AS Employee, 
+        DENSE_RANK()OVER(PARTITION BY d.id ORDER BY salary DESC) AS rnk
+    FROM Department d
+    JOIN Employee e
+    ON d.id = e.departmentId
+    )
+SELECT Department, Employee, Salary
+FROM employee_department
+WHERE rnk <= 3
+
+-- select Department , Employee , Salary from 
+-- (select d.name as Department , 
+-- e.name as Employee , 
+-- e. salary as Salary,
+-- dense_rank() over (partition by d.name order by e.salary desc) as ranks
+-- from employee e
+-- left join Department d 
+-- on e.departmentId = d.id) v
+-- where ranks <=3
