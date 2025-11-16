e69455871ea60c53c2e333e069b154dd4e307379
VIJAY ASHOK KARLAWAD <139671897+VIJAY-A-K-1405@users.noreply.github.com>
2025-11-16 11:06:47 +0530

    Time: 342 ms (50.50%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+-- The DATEADD() function adds a time/date interval to a date and then returns the date.
+-- The DATE_SUB() function subtracts a time/date interval from a date and then returns the date.
+
+
+select visited_on, 
+(
+    select sum(amount) from customer
+    where visited_on between date_sub(c.visited_on, interval 6 day)
+    and c.visited_on
+) as amount, 
+round((
+    select sum(amount)/7 from customer
+    where visited_on between date_sub(c.visited_on, interval 6 day)
+    and c.visited_on
+), 2) as average_amount
+from Customer c
+where visited_on >= (
+    select date_add(min(visited_on), interval 6 day) from customer
+)
+group by visited_on
+order by visited_on
