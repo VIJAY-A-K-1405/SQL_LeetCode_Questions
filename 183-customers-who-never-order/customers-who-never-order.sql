d1a36d673a02ee16ac9f53419e4105e3f7498cda
VIJAY ASHOK KARLAWAD <139671897+VIJAY-A-K-1405@users.noreply.github.com>
2025-11-22 12:05:01 +0530

    Time: 574 ms (25.54%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,12 +1,15 @@
 # Write your MySQL query statement below
--- SELECT c.name AS Customers
--- FROM Customers c
--- LEFT JOIN Orders o 
---     ON c.id = o.customerId
--- WHERE o.customerId IS NULL;
+-- Using JOIN is generally more efficient in SQL Server than using subqueries or multiple separate queries to achieve the same result.
 
-select name as customers
-from customers 
-where id not in (
-    select customerId from orders
-)
+-- select name as customers
+-- from customers 
+-- where id not in (
+--     select customerId from orders
+-- )
+
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o 
+ON c.id = o.customerId
+WHERE o.customerId IS NULL;
+
