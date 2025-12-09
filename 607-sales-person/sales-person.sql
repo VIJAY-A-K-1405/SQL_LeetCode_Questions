7eb5d447a4a4b534a372395fca904053b858096b
VIJAY ASHOK KARLAWAD <139671897+VIJAY-A-K-1405@users.noreply.github.com>
2025-12-10 03:11:50 +0530

    Time: 1388 ms (36.24%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,6 +1,12 @@
 # Write your MySQL query statement below
-select s.name from salesperson s
-left join orders o on o.sales_id = s.sales_id
-left join company c on c.com_id = o.com_id and c.name = 'RED'
-group by s.sales_id, s.name
-having count(c.com_id) = 0;
+-- select s.name from salesperson s
+-- left join orders o on o.sales_id = s.sales_id
+-- left join company c on c.com_id = o.com_id and c.name = 'RED'
+-- group by s.sales_id, s.name
+-- having count(c.com_id) = 0;
+
+select s.name
+from orders o
+join company c on c.com_id = o.com_id and c.name = 'red'
+right join salesperson s on s.sales_id = o.sales_id
+where o.sales_id is null
